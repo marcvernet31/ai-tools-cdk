f281f0eefa6505ca31e3319c808e4a1c6bd205ae
marcvernet31 <mrcvs31@gmail.com>
2023-10-15 14:49:19 +0200

    Initial commit

---
+++ test/ai-tools-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AiToolsCdk from '../lib/ai-tools-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ai-tools-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AiToolsCdk.AiToolsCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
